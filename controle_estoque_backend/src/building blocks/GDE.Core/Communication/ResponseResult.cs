5ec3fcfc7b707f6030a4859ada18a6e1cfb9e910
Lucas Manzi <lucas_manzi@hotmail.com>
2024-05-13 15:17:31 -0300

    feat(backend) Trocar nome colection erros

--- controle_estoque_backend/src/building blocks/GDE.Core/Communication/ResponseResult.cs
+++ controle_estoque_backend/src/building blocks/GDE.Core/Communication/ResponseResult.cs
@@ -4,12 +4,12 @@
     {
         public ResponseResult()
         {
-            Errors = new ResponseErrorMessages();
+            Erros = new ResponseErrorMessages();
         }
 
         public string? Title { get; set; }
         public int Status { get; set; }
-        public ResponseErrorMessages Errors { get; set; }
+        public ResponseErrorMessages Erros { get; set; }
     }
 
     public class ResponseErrorMessages
