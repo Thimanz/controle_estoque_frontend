fdbb6bcba853368b1d949081d9eea4ece0067cf0
Lucas Manzi <lucas_manzi@hotmail.com>
2024-10-27 10:18:53 -0300

    feat(backend) MassTransit - Funcionarios

--- controle_estoque_backend/src/services/GDE.Funcionarios.API/Configuration/MessageBusConfig.cs
+++ controle_estoque_backend/src/services/GDE.Funcionarios.API/Configuration/MessageBusConfig.cs
@@ -1,6 +1,8 @@
-﻿using GDE.Core.Utils;
-using GDE.Funcionarios.API.Services;
-using GDE.MessageBus;
+﻿using GDE.Core.Identidade;
+using GDE.Core.MessageBus;
+using GDE.Core.Messages.Integration;
+using GDE.Funcionarios.API.Consumers;
+using MassTransit;
 
 namespace GDE.Funcionarios.API.Configuration
 {
@@ -8,8 +10,32 @@ namespace GDE.Funcionarios.API.Configuration
     {
         public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
-                .AddHostedService<RegistroFuncionarioIntegrationHandler>();
+            var messageBusCredentialsSection = configuration.GetSection("MessageBusConfig");
+            services.Configure<MessageBusCredentials>(messageBusCredentialsSection);
+
+            var messageBusCredentials = messageBusCredentialsSection.Get<MessageBusCredentials>();
+
+            if (messageBusCredentials == null) throw new ArgumentNullException(nameof(messageBusCredentials));
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<UsuarioRegistradoConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(messageBusCredentials.Host, "/", h =>
+                    {
+                        h.Username(messageBusCredentials.User);
+                        h.Password(messageBusCredentials.Host);
+                    });
+
+                    cfg.ReceiveEndpoint(UsuarioRegistradoIntegrationEvent.QueueName, e =>
+                    {
+                        e.ConfigureConsumer<UsuarioRegistradoConsumer>(context);
+                    });
+                });
+            });
+
         }
     }
 }
---
+++ controle_estoque_backend/src/services/GDE.Funcionarios.API/Consumers/UsuarioRegistradoConsumer.cs
@@ -0,0 +1,33 @@
+﻿using GDE.Core.Mediator;
+using GDE.Core.Messages.Integration;
+using GDE.Funcionarios.API.Application.Commands;
+using MassTransit;
+
+namespace GDE.Funcionarios.API.Consumers
+{
+    public class UsuarioRegistradoConsumer : IConsumer<UsuarioRegistradoIntegrationEvent>
+    {
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public UsuarioRegistradoConsumer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task Consume(ConsumeContext<UsuarioRegistradoIntegrationEvent> context)
+        {
+            var funcionarioCommand = new RegistrarFuncionarioCommand(context.Message.Id,
+                                                                     context.Message.Nome,
+                                                                     context.Message.Cpf,
+                                                                     context.Message.Email);
+            
+            using var scope = _serviceProvider.CreateScope();
+
+            var mediator = scope.ServiceProvider.GetService<IMediatorHandler>();
+            var result = await mediator.EnviarComando(funcionarioCommand);
+
+            await context.RespondAsync(new ResponseMessage(result));
+        }
+    }
+}
